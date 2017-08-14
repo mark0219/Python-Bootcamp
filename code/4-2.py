5fb45aac89d31049a309ee1208ce2fa61aef9d8d
jrb28 <james.bradley@mason.wm.edu>
2017-08-14 09:50:48 -0400

    Add files via upload

--- code/4-2.py
+++ code/4-2.py
@@ -13,18 +13,16 @@ def check_data(data_in):
         valid_input = False
     return valid_input, data_in
 
-valid_input = True
-
 costVar = raw_input('Enter variable cost: ')
-valid_input, costVar = check_data(costVar)
+valid_input1, costVar = check_data(costVar)
     
 costFixed = raw_input('Enter fixed cost: ')
-valid_input, costFixed = check_data(costFixed)
+valid_input2, costFixed = check_data(costFixed)
     
 hoursLabor = raw_input('Enter labor hours: ')
-valid_input, hoursLabor = check_data(hoursLabor)
+valid_input3, hoursLabor = check_data(hoursLabor)
     
-if valid_input == True:
+if valid_input1 and valid_input2 and valid_input3  == True:
     costTotal = costFixed + hoursLabor * costVar
     print 'Total cost = ',costTotal
 else:
